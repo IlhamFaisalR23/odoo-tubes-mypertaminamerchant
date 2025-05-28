9ae93c376494a777a7f41e4167d2148cca01151a
IlhamFaisalR23 <ilhamfaisalrr@gmail.com>
2025-05-28 14:47:41 +0700

    First Commit - Seleksi Addons

---
+++ stock_extension/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Stock Extension for Auto Forecast & Reorder',
+    'version': '16.0.1.0.0',
+    'depends': ['stock'],
+    'author': 'Ilham Faisal',
+    'data': [
+        'views/product_product_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
